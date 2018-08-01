4061e5e4cce810767510b06d712a139de739e90c
MuhammadTahaAzam1997 <muhammadtahaazam@gmail.com>
2018-08-01 21:58:24 +0500

    Calculating area of rectangle from get set mthod java

---
+++ src/rectangle/Rectangle.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rectangle;
+import java.util.Scanner;
+/**
+ *
+ * @author Muhammad Taha Azam
+ */
+
+class Shape{
+
+    float length; float wid]
+    }
+    
+}
+
+public class Rectangle {
+Scanner obj=new Scanner(System.in);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Shape o=new Shape();
+        System.out.println("Enter Length");
+        
+        o.length=obj.nextFloat();
+
+    }
+    
+}
